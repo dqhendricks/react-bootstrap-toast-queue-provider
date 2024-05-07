b661453d9170eb8863fd253dcea8be59e3324834
Dustin Hendricks <dqhendricks@users.noreply.github.com>
2024-05-07 07:18:27 -0700

    added settable autohide

--- ToastQueueProvider.jsx
+++ ToastQueueProvider.jsx
@@ -24,6 +24,7 @@ export default function ToastQueueProvider({ children }) {
         title: title,
         body: body,
         variation: variation,
+        autohide: autohide,
       },
     ]);
   }
